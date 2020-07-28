7f4cc7e2f4a82ebb933346b59fb6dbcaca51565c
chris-malloy <chrismalloymusic@gmail.com>
2020-07-28 06:47:16 -0400

    Handling custom ports.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"scan-visor/pkg/routes"
 
 	// @todo figure out how to properly import other packages, probaly have to fix gopath
@@ -15,8 +16,13 @@ func main() {
 	http.HandleFunc("/", routes.Home)
 	http.HandleFunc("/health", routes.HealthCheck)
 
-	err := http.ListenAndServe(":9000", nil)
+	port := os.Getenv("API_TCP_PORT")
+	if port == "" {
+		log.Println("API_TCP_PORT not set explicitly, defaulting to 9000")
+		port = "9000"
+	}
 
+	err := http.ListenAndServe(":" + port, nil)
 	if err != nil {
 		log.Fatalf("could not start server: %s\n", err.Error())
 	}
